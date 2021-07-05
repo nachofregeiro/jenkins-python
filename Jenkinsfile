7ce100cbe36f50aecde372f2cabc1f01676602d8
Nacho Fregeiro <nachofregeiro@gmail.com>
2021-07-05 16:19:42 -0300

    Rename python script and fix string interpolation

--- Jenkinsfile
+++ Jenkinsfile
@@ -10,7 +10,7 @@ pipeline {
       }
       steps {
         script {
-          sh 'python -m venv env && . env/bin/activate && pip install -r requirements.txt && python print.py'
+          sh 'python -m venv env && . env/bin/activate && pip install -r requirements.txt && python generate.py'
         }
       }
     }
--- generate.py
+++ generate.py
@@ -5,5 +5,5 @@ from datetime import datetime
 
 url = 'http://httpbin.org/response-headers?language=Python'
 response = requests.get(url, allow_redirects=True)
-date = datetime.now().strftime("%Y_%m_%d")
-open(f'response_{date}.txt', 'wb').write(response.content)
+date = datetime.now().strftime("%Y_%m_%d-%I:%M:%S_%p")
+open('response_{}.txt'.format(date), 'wb').write(response.content)
