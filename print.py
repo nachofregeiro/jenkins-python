9c473f602ce66bdd82e5db7afb84ee212316c3fd
Nacho Fregeiro <nachofregeiro@gmail.com>
2021-07-05 15:17:18 -0300

    Remove date from file name

--- print.py
+++ print.py
@@ -6,4 +6,4 @@ from datetime import datetime
 url = 'http://httpbin.org/response-headers?language=Python'
 response = requests.get(url, allow_redirects=True)
 date = datetime.now().strftime("%Y_%m_%d-%I:%M:%S_%p")
-open(f'response_{date}.txt', 'wb').write(response.content)
+open('response.txt', 'wb').write(response.content)
