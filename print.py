0e722dfff629a5c46f7fddc8eccd15d79aa09f3f
Nacho Fregeiro <nachofregeiro@gmail.com>
2021-07-05 15:39:48 -0300

    Rename jenkins file and remove pip3 from python script

--- print.py
+++ print.py
@@ -7,4 +7,3 @@ url = 'http://httpbin.org/response-headers?language=Python'
 response = requests.get(url, allow_redirects=True)
 date = datetime.now().strftime("%Y_%m_%d-%I:%M:%S_%p")
 open('response.txt', 'wb').write(response.content)
-pip3
