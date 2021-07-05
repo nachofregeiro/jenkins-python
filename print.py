481b640060c43907eed3e0b047896c60e0998f29
Nacho Fregeiro <nachofregeiro@gmail.com>
2021-07-05 14:50:16 -0300

    Update python script to download a file over http

--- print.py
+++ print.py
@@ -1,1 +1,7 @@
-print("Hello from Python script!")
+print("Downloading report...")
+
+import requests
+
+url = 'http://httpbin.org/response-headers?language=Python'
+response = requests.get(url, allow_redirects=True)
+open('response.txt', 'wb').write(response.content)
