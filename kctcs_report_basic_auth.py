c41cc8e6c4447a416f68c009d4bec38f5e1a5e49
Nacho Fregeiro <nachofregeiro@gmail.com>
2021-07-07 12:53:56 -0300

    Add kctcs_report_basic_auth report generation

---
+++ kctcs_report_basic_auth.py
@@ -0,0 +1,106 @@
+import argparse
+import csv
+import requests
+from datetime import date
+
+stage_url = 'https://{}-stage.civitaslearning.com'
+prod_url = 'https://{}.civitaslearning.com'
+# base_url = 'http://localhost:3000'
+
+auth = '/auth/login'
+students = '/api/illume/current_students.csv'
+courses = '/api/course-insights/insights/gIndividualImpact/export.csv?limit=1000'
+
+def get_data(customer, env, product, username, password):
+  url = ''
+  if env == 'stage':
+    url = stage_url.format(customer)
+  else:
+    url = prod_url.format(customer)
+  creds = {
+    'username': username,
+    'password': password
+  }
+  resp = None
+  with requests.Session() as session:
+    print('Authenticating')
+    session.get(url + auth)
+    session.post(url + auth, data=creds)
+    auth_resp = session.get(url + auth)
+    
+    print('Gathering data')
+    if product == 'illume':
+      req_body = { 
+        '_csrf': auth_resp.cookies['csrfToken'],
+        'request':
+        '{"show_unenrolled_students":false,"addCols":["first_term"],"sort":{"col":"last_name","dir":"asc"},"feature_filters":[],"completionWindows":[{"key":"normal","value":3}]}'
+        }
+      resp = session.post(url + students, cookies=auth_resp.cookies, headers={'x-csrf-token': auth_resp.cookies['csrfToken']}, data=req_body)
+    else:
+      resp = session.get(url + courses, cookies=auth_resp.cookies, headers={'x-csrf-token': auth_resp.cookies['csrfToken']})
+    
+    if resp and resp.text:
+      return resp.text.split('\n')
+
+def format_data(data, delimiter='!~'):
+  print('Formatting data')
+  if data is None or len(data) == 0:
+    return
+  csv_data = []
+  header_line = -1
+  for c in data:
+    if len(c.split()) > 1:
+      header_line = data.index(c)
+      break
+  if (header_line >= 0):
+    boilerplate = data[0:header_line]
+    for row in data[header_line:-1]:
+      csv_data.append(tuple(e.encode('utf-8') for e in row.split(',')))
+  
+  formatted_data = []
+  for b in boilerplate:
+    formatted_data.append(b)
+  for c in csv_data:
+    formatted_data.append(delimiter.join(map(decode, c)) + '\n')
+    
+  return formatted_data
+
+def decode(c):
+  return c.decode('utf-8')
+
+def write_data(data, filestub):
+  if data is None or len(data) == 0:
+    print('No data to write')
+    return
+  today = date.today()
+  filename = '{}_{}.txt'.format(filestub, today.strftime('%Y%m%d'))
+
+  with open(filename, 'w') as outfile:
+    print('Writing data to {}'.format(filename))
+    for d in data:
+      outfile.write(d)
+
+if __name__ == '__main__':
+
+  parser = argparse.ArgumentParser()
+  parser.add_argument('env', help='Environment.',
+                      type=str,
+                      choices=('stage', 'prod'))
+  parser.add_argument('customer', help='Customer whose data you want to pull',
+                      type=str)
+  parser.add_argument('product_name', help='The product from which to pull data',
+                      type=str,
+                      choices=('illume', 'courses'))
+  parser.add_argument('username', help='Username for basic auth account on customer illume site',
+                      type=str)
+  parser.add_argument('password', help='Password for basic auth account on customer illume site',
+                      type=str)
+
+  args = parser.parse_args()
+
+  data = get_data(args.customer, args.env, args.product_name, args.username, args.password)
+  formatted_data = format_data(data)
+  if args.product_name == 'illume':
+    write_data(formatted_data, 'illume_daily_file')
+  else:
+    write_data(formatted_data, 'illume_courses_daily_file')
